2785f023f85e47fc66d0c520994a0cb48f73eba9
Feng <z332007851@163.com>
2025-08-12 09:44:58 +0800

    Update sync.sh

--- .github/workflows/sync.sh
+++ .github/workflows/sync.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Only need to sync images that changed in current commit
-FILES=$(find online -type f)
+#FILES=$(find online -type f)
 
-for FILE in $FILES
-do
-     echo "========> Syncing: $FILES <========"
-    ./image-syncer --auth=./auth.yaml --proc=3 --retries=5 --images=${FILE}
-done
+#for FILE in $FILES
+#do
+    # echo "========> Syncing: $FILES <========"
+./image-syncer --auth=./auth.yaml --proc=3 --retries=5 --images=online/tencent.yaml
+#done
